805576315d589d5c97a48c6e3ae5fb78aac5f6c0
Robert076 <robertberes06@gmail.com>
2024-10-06 20:37:42 +0300

    .

---
+++ theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+    inputPadding: '5px 15px',
+    inputMarginBottom: '10px',
+    fontSizeNormal: '16px',
+    buttonPadding: '5px 15px',
+    borderRadiusNormal: '10px',
+}
